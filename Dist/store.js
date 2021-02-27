ff021b0c6607f0d58fdbc51aa81b1a910765c1f8
Connor Lewis <connorlewis128@yahoo.com>
2021-02-27 17:56:31 -0500

    change product and add images

--- Dist/store.js
+++ Dist/store.js
@@ -7,8 +7,8 @@
 let carts = document.querySelectorAll('#add-to-cart');
 let products = [
   {
-    name:'Suliven Quilted Jacket',
-    Tag:'brown jacket',
+    name:'ZIPPERED HOODED SWEATSHIRT',
+    Tag:'black jacket/front',
     price:680.00,
     inCart:0
   }
